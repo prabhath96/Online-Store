572d17291e2161fec8087ce1b91d7cf342e9e334
prabhath96 <124553714+prabhath96@users.noreply.github.com>
2023-04-10 15:09:46 +0530

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,31 @@
+// models/Product.js
+
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
